dfb44d93c2741d82c07d886286f7ffbd80ae23a8
Nart Tlisha <daniel.abzakh@gmail.com>
2021-10-20 17:33:04 +0300

    Added cleanup script

---
+++ scripts/process-text.sh
@@ -0,0 +1,11 @@
+for file in *.txt;
+do
+  sed -i -r 's/[0-9]*([[:alpha:]–])[0-9]*/\1/g' $file;
+  sed -i -z 's/[\xC2\xAD-]\n//g' $file;
+  sed -i -e 's/\xC2\xAD//g' $file;
+  sed -i -z 's/\n/ /g' $file;
+  sed -i -r 's/([:!?]) /\1\n/g' $file;
+  sed -i -r 's/([[:alpha:]][[:alpha:]][[:alpha:]]\.) /\1\n/g' $file;
+  sort $file | uniq > $file+2
+  cat $file+2 | awk '{ print length, $0 }' | sort -n -s | cut -d" " -f2- > $file
+done
